f7236a291de42def7e852fec86aa2408d0de8d2f
zouh9907 <zouh@rose-hulman.edu>
2017-12-12 17:35:41 -0500

    finish m1

--- src/m1r_mutation_vs_reassignment.py
+++ src/m1r_mutation_vs_reassignment.py
@@ -5,7 +5,7 @@ This module demonstrates the distinction betweeen:
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
          their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -17,12 +17,12 @@ import rosegraphics as rg
 
 def main():
     """ Runs several examples. """
-#     example1()
-#     example2()
+    example1()
+    example2()
 
 
 # ----------------------------------------------------------------------
-# TODO: 2.
+# DONE: 2.
 #  Step a:  READ the code in  example1.
 #
 #  Step b:  PREDICT what  example1  will print when it runs.
@@ -61,7 +61,7 @@ def example1():
 
 
 # ----------------------------------------------------------------------
-# TODO: 3.
+# DONE: 3.
 #  Step a:  READ the code in  example2.
 #           Pay close attention to the difference between the functions:
 #              mutate_point
